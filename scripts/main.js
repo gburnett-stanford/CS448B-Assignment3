afab0400e5d825f9f423ed50650befaa45dfd1d3
Gary Burnett <garyb@stanford.edu>
2021-10-20 22:08:42 -0700

    fix: intersection works, but is slow

--- scripts/main.js
+++ scripts/main.js
@@ -44,172 +44,193 @@ restaurantData = d3.csv('data/restaurant_data.csv', function(d) {
         x: +x_projection,
         y: +y_projection
     }; 
-      }).then(function(restaurantData) {
+      }).then(drawLocationPins) 
 
-    // Draw a search area as a circle
-    svg.append('circle')
-      .attr('class', 'radius')
-      .attr('id', 'circle-a')
-      .attr('r', 50)
-      .attr('cx', 100)
-      .attr('cy', 100)
-      .attr('fill', 'black')
-      .attr('opacity', 0.3)
+function drawLocationPins(restaurantData){
+  // Draw a search area as a circle
+  svg.append('circle')
+    .attr('class', 'radius')
+    .attr('id', 'circle-a')
+    .attr('r', 50)
+    .attr('cx', 100)
+    .attr('cy', 100)
+    .attr('fill', 'black')
+    .attr('opacity', 0.3)
 
-    // Draw a search area as a circle 
-    svg.append('circle')
-      .attr('class', 'radius')
-      .attr('id', 'circle-b')
-      .attr('r', 50)
-      .attr('cx', 200)
-      .attr('cy', 200)
-      .attr('fill', 'black')
-      .attr('opacity', 0.3)
+  // Draw a search area as a circle 
+  svg.append('circle')
+    .attr('class', 'radius')
+    .attr('id', 'circle-b')
+    .attr('r', 50)
+    .attr('cx', 200)
+    .attr('cy', 200)
+    .attr('fill', 'black')
+    .attr('opacity', 0.3)
 
+  // Draw location pins as circles 
+  svg.selectAll('.location_pin')
+    .data(restaurantData)
+    .join('circle')
+    .attr('r', 3)
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y)
+    .attr('fill', 'grey')
+    .attr('opacity', 0.3)
+    .attr('class', 'location_pin')
+
+  updateLocationPins()
+
+  // determine if this point is intersecting with the radii or not
+  function intersecting(){
+
+    // retrieve the objects for the circles and location pin
+    circle_a = d3.select('#circle-a')
+    circle_b = d3.select('#circle-b')
+    location_pin = d3.select(this)
+
+    // create objects representing the centers 
+    circle_a_center = {x: Number(circle_a.attr('cx')), y: Number(circle_a.attr('cy'))}
+    circle_b_center = {x: Number(circle_b.attr('cx')), y: Number(circle_b.attr('cy'))}
+    location_pin_center = {x: Number(location_pin.attr('cx')), y: Number(location_pin.attr('cy'))}
+
+    // get the current radius of each circle 
+    circle_a_radius = Number(circle_a.attr('r'))
+    circle_b_radius = Number(circle_b.attr('r'))
+
+    // calculate the distance from the location pin to each circle 
+    distance_to_a = distance(circle_a_center, location_pin_center)
+    distance_to_b = distance(circle_b_center, location_pin_center)
+
+    return (distance_to_a<=circle_a_radius && distance_to_b<=circle_b_radius)
+
+  }
+
+  function updateLocationPins(){
+
+    // update which points are intersecting and which ones are not 
     svg.selectAll('.location_pin')
-      .data(restaurantData)
-      .join('circle')
-      .attr('r', 3)
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y)
       .attr('fill', 'grey')
-      .attr('opacity', 0.3)
-      .classed('location_pin', true)
+      .on('mouseover', null)
+      .on('mouseout', null)
       .classed('intersecting_point', intersecting)
-
-    // determine if this point is intersecting with the radii or not
-    function intersecting(){
-
-      // retrieve the objects for the circles and location pin
-      circle_a = d3.select('#circle-a')
-      location_pin = d3.select(this)
-
-      // create objects representing the centers 
-      circle_a_center = {x: Number(circle_a.attr('cx')), y: Number(circle_a.attr('cy'))}
-      location_pin_center = {x: Number(location_pin.attr('cx')), y: Number(location_pin.attr('cy'))}
-
-      // get the current radius of each circle 
-      circle_a_radius = Number(circle_a.attr('r'))
-
-      // calculate the distance from the location pin to each circle 
-      distance_to_a = distance(circle_a_center, location_pin_center)
-
-      console.log(circle_a_radius)
-
-      return distance_to_a<=circle_a_radius
-
-    }
 
     svg.selectAll('.intersecting_point')
       .attr('fill', 'steelblue')
       .on('mouseover', mouseOver)
       .on('mouseout', mouseOut)
+      
+  }
 
-    // ***** DEFINE BEHAVIOR FOR HOVERING OVER LOCATION POINTS ***** 
+  // ***** DEFINE BEHAVIOR FOR HOVERING OVER LOCATION POINTS ***** 
 
-    function mouseOver(event, d){
-      d3.select(this)
-        .style("fill", "green")
-        .style("stroke", "black")
+  function mouseOver(event, d){
+    d3.select(this)
+      .style("fill", "green")
+      .style("stroke", "black")
 
-      // Label for Name, Grade, and Score of each place
-      // Split into three 'text' labels for spacing
-      svg.append('text')
-        .attr('class', 'ptLabel')
-        .attr('x', d.x + LABEL_MARGIN)
-        .attr('y', d.y)
-        .text(`Name: ${d.name}`)
-      svg.append('text')
-        .attr('class', 'ptLabel')
-        .attr('x', d.x + LABEL_MARGIN)
-        .attr('y', d.y + LABEL_MARGIN)
-        .text(`Grade: ${d.grade}`)
-      svg.append('text')
-        .attr('class', 'ptLabel')
-        .attr('x', d.x + LABEL_MARGIN)
-        .attr('y', d.y + 2 * LABEL_MARGIN)
-        .text(`Score: ${d.score}`)
-    }
+    // Label for Name, Grade, and Score of each place
+    // Split into three 'text' labels for spacing
+    svg.append('text')
+      .attr('class', 'ptLabel')
+      .attr('x', d.x + LABEL_MARGIN)
+      .attr('y', d.y)
+      .text(`Name: ${d.name}`)
+    svg.append('text')
+      .attr('class', 'ptLabel')
+      .attr('x', d.x + LABEL_MARGIN)
+      .attr('y', d.y + LABEL_MARGIN)
+      .text(`Grade: ${d.grade}`)
+    svg.append('text')
+      .attr('class', 'ptLabel')
+      .attr('x', d.x + LABEL_MARGIN)
+      .attr('y', d.y + 2 * LABEL_MARGIN)
+      .text(`Score: ${d.score}`)
+  }
 
-    function mouseOut(){
-      d3.select(this)
-        .style("fill", "steelblue")
-        .style("stroke", "none")
-      svg.selectAll('.ptLabel').remove() // remove all
-    }
+  function mouseOut(){
+    d3.select(this)
+      .style("fill", "steelblue")
+      .style("stroke", "none")
+    svg.selectAll('.ptLabel').remove() // remove all
+  }
 
-    // ***** DEFINE BEHAVIOR FOR DRAGGING THE SEARCH AREA ***** 
+  // ***** DEFINE BEHAVIOR FOR DRAGGING THE SEARCH AREA ***** 
 
-    // Behavior at the start of the drag (clicking on the search area): 
-    // 1. Change the stroke to red, for visual feedback 
-    function dragStart(){
-      d3.select(this)
-        .attr('stroke', 'red')
-        .attr('stroke-width', '2')
-    }
+  // Behavior at the start of the drag (clicking on the search area): 
+  // 1. Change the stroke to red, for visual feedback 
+  function dragStart(){
+    d3.select(this)
+      .attr('stroke', 'red')
+      .attr('stroke-width', '2')
+  }
 
-    // Behavior while the search area is being dragged:  
-    // 1. Move the x and y position of the search area 
-    // 2. Don't let users drag the search area out of bounds 
-    function dragMove(event){
+  // Behavior while the search area is being dragged:  
+  // 1. Move the x and y position of the search area 
+  // 2. Don't let users drag the search area out of bounds 
+  function dragMove(event){
 
-      // the radius of the search area is ever changing so 
-      // we need to pull the value directly from the object 
-      radius = d3.select(this).attr('r')
+    // the radius of the search area is ever changing so 
+    // we need to pull the value directly from the object 
+    radius = d3.select(this).attr('r')
 
-      // defining new x and y values for the search area, to make sure
-      // they stay within the bounds of the map 
-      bounded_cx = Math.max(radius, Math.min(mapWidth-radius, event.x));
-      bounded_cy = Math.max(radius, Math.min(mapHeight-radius, event.y));
+    // defining new x and y values for the search area, to make sure
+    // they stay within the bounds of the map 
+    bounded_cx = Math.max(radius, Math.min(mapWidth-radius, event.x));
+    bounded_cy = Math.max(radius, Math.min(mapHeight-radius, event.y));
 
-      // set the new x and y positions
-      d3.select(this)
-        .attr('cx', bounded_cx)
-        .attr('cy', bounded_cy)
-    }
+    // set the new x and y positions
+    d3.select(this)
+      .attr('cx', bounded_cx)
+      .attr('cy', bounded_cy)
 
-    // Behavior when the search area is no longer being dragged:  
-    // 1. Remove the stroke 
-    function dragEnd(){
-      d3.select(this)
-        .attr('stroke', null)
-        .attr('stroke-width', null)
-    }
+    // update location pins 
+    updateLocationPins()
+  }
 
-    // Create a handler object to descbribe the dragging behavior 
-    var drag_handler = d3.drag()
-        .on('start', dragStart)
-        .on('drag', dragMove)
-        .on('end', dragEnd)
+  // Behavior when the search area is no longer being dragged:  
+  // 1. Remove the stroke 
+  function dragEnd(){
+    d3.select(this)
+      .attr('stroke', null)
+      .attr('stroke-width', null)
+  }
 
-    // Apply the handler to the radius objects 
-    drag_handler(svg.selectAll('.radius'))
+  // Create a handler object to descbribe the dragging behavior 
+  var drag_handler = d3.drag()
+      .on('start', dragStart)
+      .on('drag', dragMove)
+      .on('end', dragEnd)
 
-    // ***** RADIUS SLIDER *****
+  // Apply the handler to the radius objects 
+  drag_handler(svg.selectAll('.radius'))
 
-    // A function that update the chart when slider is moved
-    var circleRadius = d3.selectAll('.radius')
+  // ***** RADIUS SLIDER *****
 
-    function updateRadius(updatedRadius) {
-      circleRadius.transition()
-      .ease(d3.easeLinear)
-        .duration(200)
-        .delay(10)
-        .attr("r", updatedRadius)
-    }
+  // A function that update the chart when slider is moved
+  var circleRadius = d3.selectAll('.radius')
 
-    // Listen to the slider
-    d3.select("#radius-slider").on("change", function(d){
-      selectedValue = this.value
-      updateRadius(selectedValue)
-    });
+  function updateRadius(updatedRadius) {
+    circleRadius.transition()
+    .ease(d3.easeLinear)
+      .duration(200)
+      .delay(10)
+      .attr("r", updatedRadius)
+  }
 
-    // ***** CREATE FUNCTIONS FOR INTERSECTION *****
+  // Listen to the slider
+  d3.select("#radius-slider").on("change", function(d){
+    selectedValue = this.value
+    updateRadius(selectedValue)
+    updateLocationPins()
+  });
 
-    // calculates the distance between two points p1 and p2 
-    function distance(p1, p2){
-      dx = p1.x - p2.x 
-      dy = p1.y - p2.y
-      return Math.sqrt(dx * dx + dy * dy)
-    }
-  })
+  // ***** CREATE FUNCTIONS FOR INTERSECTION *****
+
+  // calculates the distance between two points p1 and p2 
+  function distance(p1, p2){
+    dx = p1.x - p2.x 
+    dy = p1.y - p2.y
+    return Math.sqrt(dx * dx + dy * dy)
+  }
+
+  }
